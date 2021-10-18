de54aaf6ad8fe59ef34c5c13fc58b507483b68b0
derick-droid <derrickokinda9@gmail.com>
2021-10-18 16:46:25 +0300

    python exercise coding bat

---
+++ warm up 2/front_times/front_times.py
@@ -0,0 +1,21 @@
+"""
+Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or
+whatever is there if the string is less than length 3. Return n copies of the front;
+
+front_times('Chocolate', 2) → 'ChoCho'
+front_times('Chocolate', 3) → 'ChoChoCho'
+front_times('Abc', 3) → 'AbcAbcAbc'
+
+"""
+
+
+def front_times(str, n):
+    if n < 0:
+        return False
+    else:
+        if len(str) < 3:
+            return (str * n)
+        else:
+            first = str[:3]
+            return (first * n)
+
