63674782503f22b2558a40a35f180085f2e88d6c
derick-droid <derrickokinda9@gmail.com>
2021-10-18 10:37:13 +0300

    python beginner exercise in coding bat website

---
+++ warm up 1/sleep in/sleep_in.py
@@ -0,0 +1,25 @@
+"""
+The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on
+vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+sleep_in(False, False) → True
+sleep_in(True, False) → False
+sleep_in(False, True) → True
+
+"""
+
+
+def sleep_in(weekday, vacation):
+    if weekday == True and vacation == True:
+        return True
+    elif weekday == True and vacation == False:
+        return False
+    elif weekday == False and vacation == True:
+        return True
+
+    elif weekday == False and vacation == False:
+        return True
+
+    return True
+
+
+test = sleep_in(True, False)
