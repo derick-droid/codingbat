11cc0a5fc5fc7ed8eae7325ae2f9a353f62398fb
derick-droid <derrickokinda9@gmail.com>
2021-10-25 10:53:50 +0300

    python practices in coding bat

---
+++ warm up 2/first_two/main.py
@@ -0,0 +1,18 @@
+"""
+ Given a string, return the string made of its first two chars, so the String "Hello" yields "He".
+If the string is shorter than length 2, return whatever there is, so "X" yields "X",
+and the empty string "" yields the empty string "".
+
+first_two('Hello') → 'He'
+first_two('abcdefg') → 'ab'
+first_two('ab') → 'ab'
+
+"""
+
+
+def first_two(word):
+    if len(word) < 2:
+        return word
+    else:
+        return (word[:2])
+
