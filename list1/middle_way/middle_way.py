1d4d9f7b72ca2e115143a9dfd3801f38339aff85
derick-droid <derrickokinda9@gmail.com>
2021-10-25 14:29:29 +0300

    python practical programs in coding bat

---
+++ list1/middle_way/middle_way.py
@@ -0,0 +1,15 @@
+"""
+Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+
+middle_way([1, 2, 3], [4, 5, 6]) → [2, 5]
+middle_way([7, 7, 7], [3, 8, 0]) → [7, 8]
+middle_way([5, 2, 9], [1, 4, 5]) → [2, 4]
+
+"""
+
+
+def middle_way(a, b):
+    if len(a) == 3 and len(b) == 3:
+        return ([a[1], b[1]])
+    else:
+        return False
