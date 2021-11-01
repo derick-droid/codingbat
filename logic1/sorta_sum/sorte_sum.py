6ed76dd35f5bbcf61cb0b3b9a9f40fcbc51c30f7
derick-droid <derrickokinda9@gmail.com>
2021-11-01 08:53:34 +0300

    python practice program in coding bat

---
+++ logic1/sorta_sum/sorte_sum.py
@@ -0,0 +1,17 @@
+"""
+Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden,
+so in that case just return 20.
+
+sorta_sum(3, 4) → 7
+sorta_sum(9, 4) → 20
+sorta_sum(10, 11) → 21
+"""
+
+
+def sorta_sum(a, b):
+    summation = a + b
+    if summation in range(10, 20):
+        return 20
+    else:
+        return summation
+
