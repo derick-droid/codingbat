04f1539979594ac2b81a09508d14870b4e61cf50
derick-droid <derrickokinda9@gmail.com>
2021-11-01 09:09:10 +0300

    python practical practices in coding bat

---
+++ logic1/alrm_clock/alarm_clock.py
@@ -0,0 +1,30 @@
+"""
+Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we are on vacation, return
+a string of the form "7:00" indicating when the alarm clock should ring. Weekdays, the alarm should be "7:00" and on the
+weekend it should be "10:00". Unless we are on vacation -- then on weekdays it should be "10:00" and weekends it should
+be "off".
+
+alarm_clock(1, False) → '7:00'
+alarm_clock(5, False) → '7:00'
+alarm_clock(0, False) → '10:00'
+
+"""
+
+
+def alarm_clock(day, vacation):
+    if (vacation == True):
+        if day in range(1, 6):
+            return "10:00"
+        elif day == 0 or day == 6:
+            return "off"
+        else:
+            return "invalid day of the week"
+    else:
+        if day in range(1, 6):
+            return "7:00"
+        elif day == 0 or day == 6:
+            return "10:00"
+        else:
+            return "invalid day of the week"
+
+
