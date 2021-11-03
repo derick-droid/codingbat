7735264e5475fde3ebb8acee2242d7615b518e39
derick-droid <derrickokinda9@gmail.com>
2021-11-03 14:29:28 +0300

    python practical program in codingbat

---
+++ string2/xyz_there/xyz_there.py
@@ -0,0 +1,23 @@
+"""
+Return True if the given string contains an appearance of "xyz" where the xyz is not directly preceeded
+by a period (.). So "xxyz" counts but "x.xyz" does not.
+
+xyz_there('abcxyz') → True
+xyz_there('abc.xyz') → False
+xyz_there('xyz.abc') → True
+"""
+
+
+def xyz_there(str):
+    if str[:3] == "xyz":
+        return True
+
+    for i in range(1, len(str) - 2):
+        if str[i - 1] != "." and str[i:i + 3] == "xyz":
+            return True
+
+    return False
+
+
+
+s = xyz_there("xyz")
