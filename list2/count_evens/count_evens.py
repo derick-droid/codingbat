84d310f4adee475335c310f54905d5fe491e0223
derick-droid <derrickokinda9@gmail.com>
2021-11-03 14:54:55 +0300

    python practicals in codingbat

---
+++ list2/count_evens/count_evens.py
@@ -0,0 +1,16 @@
+"""
+Return the number of even ints in the given array. Note:
+the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
+
+count_evens([2, 1, 2, 3, 4]) → 3
+count_evens([2, 2, 0]) → 3
+count_evens([1, 3, 5]) → 0
+
+"""
+def count_evens(num):
+    count = 0
+    for number in num:
+        if number % 2 == 0:
+            count += 1
+    return count
+
